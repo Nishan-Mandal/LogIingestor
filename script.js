d315d6995b69b59d53a444cfe04c3f1ef5f7e648
Nishan-Mandal <76816227+Nishan-Mandal@users.noreply.github.com>
2023-11-19 21:43:23 +0530

    v2

--- script.js
+++ script.js
@@ -1,85 +1,182 @@
-// let p = fetch("https://script.google.com/macros/s/AKfycbyauTg_YlAz_8vdwg8W8Ebkr3dFyAUt62-pcVNX-PfdpWPKtVWNDFt-BNEIYTjmxjZF/exec?action=getLog")
-// p.then((response) => {
-//         console.log(response.status)
-//         console.log(response.ok)
-//         console.log(response.headers)
-//         return response.json()
-// }).then((value2) => {
-//         console.log(value2)
-// })
 
-
+/**
+ * Builds an array of conditions based on user input and calls the queryData function.
+ * @throws {MyCustomError} Throws an error if no conditions are selected.
+ */
 function buildConditionsArray() {
-        var conditions = [];
+        try {
+                var conditions = [];
 
-        // Get all elements with the class 'horizontal-container'
-        var containers = document.getElementsByClassName('horizontal-container');
+                // Get all elements with the class 'horizontal-container'
+                var containers = document.getElementsByClassName('horizontal-container');
 
-        // Iterate through each container
-        for (var i = 0; i < containers.length; i++) {
-                var container = containers[i];
+                // Iterate through each container
+                for (var i = 0; i < containers.length; i++) {
+                        var container = containers[i];
 
-                // Check if the checkbox in the container is checked
-                var checkbox = container.querySelector('input[type="checkbox"]');
-                if (checkbox.checked) {
-                // If checked, build the condition object
-                var label = container.querySelector('label');
-                var column = label.getAttribute('for');
-                var operator = container.querySelector('select').value;
-                var value = container.querySelector('input[type="text"]').value;
+                        // Check if the checkbox in the container is checked
+                        var checkbox = container.querySelector('input[type="checkbox"]');
+                        if (checkbox && checkbox.checked) {
+                                // If checked, build the condition object
+                                var label = container.querySelector('label');
+                                var column = label.getAttribute('for');
+                                var operator = container.querySelector('select').value;
+                                // Check if the operator is 'BETWEEN'
+                                if (operator === 'BETWEEN') {
+                                        // For 'BETWEEN', get both input values
+                                        var value1 = container.querySelector('input[type="text"]').value;
+                                        var value2 = container.querySelector('input[type="text"]:last-child').value;
 
-                conditions.push({ columnIndex: column, operator: operator, value: value });
+                                        // Add both values to the conditions array
+                                        conditions.push({ columnIndex: column, operator: operator, value: [value1, value2] });
+                                } else {
+                                        // For other operators, get the single input value
+                                        var value = container.querySelector('input[type="text"]').value;
+
+                                        // Add the value to the conditions array
+                                        conditions.push({ columnIndex: column, operator: operator, value: value });
+                                }
+                        }
+                }
+
+                // Check if conditions array is empty
+                if (conditions.length === 0) {
+                        throw new MyCustomError('Error: No conditions selected! Please select atleast one filter');
+                }
+
+                // Clear any previous error message
+                document.getElementById('message').innerText = '';
+
+                // Calling queryData function with the conditions
+                queryData(conditions);
+        } catch (error) {
+                document.getElementById('message').innerText = error.message;
+        }
+}
+
+/**
+ * Makes a request to a Google Apps Script API with specified conditions and updates the response table.
+ * @param {Array} conditions - An array of conditions for filtering data.
+ */
+function queryData(conditions) {
+        const responseTableElement = document.getElementById('responseTable').innerHTML = '';
+        const encodedConditions = encodeURIComponent(JSON.stringify(conditions));
+        const apiUrl = `https://script.google.com/macros/s/AKfycbzD_y7qWBu0YAQyrDxuP6QnLXCyc2lb73-G8veVDnA5Wf6S4-wuigoMm6fjKDtXLmAo/exec?conditions=${encodedConditions}`;
+        //Make callout
+        sendRequest(apiUrl);
+}
+
+/**
+ * Executes an advanced query with the specified text, updating the response table.
+ * Throws an error if the text is empty.
+ */
+function advancedQuery() {
+        try {
+                document.getElementById('message').innerText = '';
+                document.getElementById('responseTable').innerHTML = '';
+                var text = document.getElementById('globalText').value;
+                console.log(text);
+                if (text == '' || text == null || text == undefined) {
+                        throw new MyCustomError('Error: Please enter the value in the text box');
+                }
+                const apiUrl = `https://script.google.com/macros/s/AKfycbzD_y7qWBu0YAQyrDxuP6QnLXCyc2lb73-G8veVDnA5Wf6S4-wuigoMm6fjKDtXLmAo/exec?text=${text}`;
+                sendRequest(apiUrl);
+        } catch (error) {
+                document.getElementById('message').innerText = error.message;
+        }
+}
+
+/**
+ * Sends a GET request to the specified API URL and handles the response.
+ * @param {string} apiUrl - The URL to send the GET request to.
+ */
+function sendRequest(apiUrl) {
+        // Make the GET request
+        fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                        // Handle the response data here
+                        console.log('API Response:', data);
+                        displayResponseTable(data);
+                })
+                .catch(error => {
+                        // Handle errors
+                        console.error('API Error:', error);
+                });
+}
+
+
+/**
+ * Displays the API response data in an HTML table.
+ * @param {Object[]} responseData - The array of objects containing API response data.
+ */
+function displayResponseTable(responseData) {
+        // Get the <table> element by its id
+        const responseTableElement = document.getElementById('responseTable');
+
+        // Define the desired column names
+        const columnNames = [
+                'level',
+                'message',
+                'resourceId',
+                'timestamp',
+                'traceId',
+                'spanId',
+                'commit',
+                'metadata-parentResourceId'
+        ];
+
+        // Create a table header row
+        const headerRow = responseTableElement.insertRow(0);
+        for (const columnName of columnNames) {
+                const headerCell = headerRow.insertCell();
+                headerCell.textContent = columnName;
+                headerCell.style.fontWeight = 'bold';
+        }
+
+        // Create table data rows
+        for (const response of responseData) {
+                const dataRow = responseTableElement.insertRow();
+                for (const key in response) {
+                        const dataCell = dataRow.insertCell();
+                        dataCell.textContent = response[key];
                 }
         }
-
-        // Log the resulting array
-        console.log(conditions);
-        queryData();
-        mainFunc()
 }
 
-function queryData(){
-        var conditions = [
-                { columnIndex: 0, operator: '==', value: 'error6' }
-              ];
-//       var apiUrl = 'https://script.google.com/macros/s/AKfycbx_amXsaAFxBugcmqBGPa6WaJgXlLxNJun_mHuDpH5YW8DddDi3GuzyW3IY7E6gcbAR/exec?action=getLog';
-//         // Make the API call
-//       fetch(apiUrl, {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(conditions),
-//       })
-//         .then(response => response.json())
-//         .then(data => {
-//           // Handle the response data here
-//           console.log('API Response:', data);
-//         })
-//         .catch(error => {
-//           // Handle errors
-//           console.error('API Error:', error);
-//         });
 
-}
+/**
+ * Handles the change event of the operator select element and dynamically adjusts
+ * the number of text input fields based on the selected operator.
+ * @param {HTMLSelectElement} operatorSelect - The HTML select element for operators.
+ */
+function handleOperatorChange(operatorSelect) {
+        var textContainer = document.getElementById('timestampTextContainer');
+        var selectedOperator = operatorSelect.value;
 
-const createTodo = async (todo) => {
-        let options = {
-                method: "POST",
-                headers: {
-                        "Content-type": "application/json"
-                },
-                body: JSON.stringify(todo),
+        // If the selected operator is 'BETWEEN', show two text fields
+        if (selectedOperator === 'BETWEEN') {
+                textContainer.innerHTML = `
+            <input type="text" id="timestampTextStart" name="timestampTextStart" placeholder="Start">
+            <input type="text" id="timestampTextEnd" name="timestampTextEnd" placeholder="End">
+          `;
+        } else {
+                // Otherwise, show a single text field
+                textContainer.innerHTML = `
+            <input type="text" id="timestampText" name="timestampText" placeholder="Enter text">
+          `;
         }
-        let p = await fetch('https://script.google.com/macros/s/AKfycbxylaRp6XHQWMxi6Js8JZXlijlezzbQ_RC21gIzhjc6_FmdShR380Ks7Ef2Gqa3edda/exec', options)
-        let response = await p.json()
-        return response
 }
 
-const mainFunc = async () => {
-        var conditions = [
-                { columnIndex: 0, operator: '==', value: 'error6' }
-              ];
-        let todor = await createTodo(conditions)
-        console.log(todor)
+/**
+ * Custom error class for handling specific error scenarios.
+ * @class MyCustomError
+ * @extends {Error}
+ * @param {string} message - The error message.
+ */
+class MyCustomError extends Error {
+        constructor(message) {
+                super(message);
+                this.name = 'MyCustomError';
+        }
 }
